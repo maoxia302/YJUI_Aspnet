6b3e18bd17a1db7a362638cd04cf9d88025f7516
maoxia302 <maoxia302@163.com>
2019-10-31 16:03:09 +0800

    导入修改

---
+++ Common/ExcelHelper.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Web;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace YJUI.Common
+{
+    /// <summary>
+    /// 导出Excel帮助类
+    /// </summary>
+    public class ExcelHelper
+    {
+        /// <summary>
+        /// Excel检查版本
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        private static string ConnectionString(string fileName)
+        {
+            bool isExcel2003 = fileName.EndsWith(".xls");
+            string connectionString = string.Format(
+                isExcel2003
+                    ? "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties=Excel 8.0;"
+                    : "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0 Xml;HDR=YES\"",
+                fileName);
+            return connectionString;
+        }
+        /// <summary>
+        /// Excel导入数据源
+        /// </summary>
+        /// <param name="sheet">sheet</param>
+        /// <param name="filename">文件路径</param>
+        /// <returns></returns>
+        public static DataTable ExcelToDataSet(string sheet, string filename)
+        {
+            try
+            {
+                DataSet ds;
+                OleDbConnection myConn = new OleDbConnection(ConnectionString(filename));
+                string strCom = " SELECT * FROM [" + sheet + "$]";
+                myConn.Open();
+                OleDbDataAdapter myCommand = new OleDbDataAdapter(strCom, myConn);
+                ds = new DataSet();
+                myCommand.Fill(ds);
+                myConn.Close();
+                return ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        public static void ExportToCsv(DataTable dt, string strHeaderText, string fileName)
+        {
+            StringBuilder sb=new StringBuilder();
+            int columns = dt.Columns.Count;
+            string[] splitHeader = strHeaderText.Split(',');
+            
+            int headerlen = splitHeader.Length;
+
+            int i = 0;
+
+            for (i = 0; i <=dt.Columns.Count-1; i++)
+            {
+                if (i > 0)
+                {
+                    sb.Append(",");
+                }
+                if (i < headerlen)
+                {
+                    sb.Append(splitHeader[i]);
+                }
+                else
+                {
+                    sb.Append("未添加标题");
+                }
+
+            }
+            sb.Append("\n");
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                for ( i = 0; i <=dt.Columns.Count-1; i++)
+                {
+                    if (i > 0)
+                    {
+                        sb.Append(",");
+                       
+                    }
+                    sb.Append(dr[i].ToString());
+
+                }sb.Append("\n");
+            }
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(sb.ToString());byte[] outBuffer=new byte[buffer.Length+3];
+            outBuffer[0] = (byte)0xEF;
+            outBuffer[1] = (byte)0xBB;
+            outBuffer[2] = (byte)0xBF;
+            Array.Copy(buffer,0,outBuffer,3,buffer.Length);
+
+            HttpResponse rs= System.Web.HttpContext.Current.Response;
+            rs.ContentEncoding = System.Text.Encoding.UTF8;
+            rs.AppendHeader("Content-Disposition","attachment;filename="+fileName);
+            rs.ContentType = "application/ms-excel";
+            rs.Write(Encoding.UTF8.GetString(outBuffer));
+            rs.Flush();
+            rs.End();
+        }
+
+
+        /// <summary>
+        /// Excel导入成Datable
+        /// </summary>
+        /// <param name="file">导入路径(包含文件名与扩展名)</param>
+        /// <returns></returns>
+        public static  DataTable ExcelToTable(string file)
+        {
+            DataTable dt=new DataTable();
+            IWorkbook workbook;
+            var extension = Path.GetExtension(file);
+            if (extension != null)
+            {
+                string fileExt = extension.ToLower();
+                using (FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read))
+                {
+                    if (fileExt == ".xlsx") { workbook = new XSSFWorkbook(fs); } else if (fileExt == ".xls") { workbook = new HSSFWorkbook(fs); } else { workbook = null; }
+                    if (workbook == null) { return null; }
+                    ISheet sheet = workbook.GetSheetAt(0);
+                    //表头
+                    IRow header = sheet.GetRow(sheet.FirstRowNum);
+                    List<int> columns = new List<int>();
+                    for (int i = 0; i < header.LastCellNum; i++)
+                    {
+                        object obj = GetValueType(header.GetCell(i));
+                        if (obj == null || obj.ToString() == string.Empty)
+                        {
+                            dt.Columns.Add(new DataColumn("Columns" + i.ToString()));
+                        }
+                        else
+                            dt.Columns.Add(new DataColumn(obj.ToString()));
+                        columns.Add(i);
+                    }
+                    for (int i = sheet.FirstRowNum + 1; i <= sheet.LastRowNum; i++)
+                    {
+                        DataRow dr = dt.NewRow();
+                        bool hasValue = false;
+                        foreach (int j in columns)
+                        {
+                            dr[j] = GetValueType(sheet.GetRow(i).GetCell(j));
+                            if (dr[j] != null && dr[j].ToString() != string.Empty)
+                            {
+                                hasValue = true;
+                            }
+                        }
+                        if (hasValue)
+                        {
+                            dt.Rows.Add(dr);
+                        }
+                    }
+
+                }
+            }
+            return dt;
+        }
+        /// <summary>
+        /// 获取单元格类型
+        /// </summary>
+        /// <param name="cell"></param>
+        /// <returns></returns>
+        private static object GetValueType(ICell cell)
+        {
+            if (cell == null)
+                return null;
+            switch (cell.CellType)
+            {
+                case CellType.Blank: //BLANK:  
+                    return null;
+                case CellType.Boolean: //BOOLEAN:  
+                    return cell.BooleanCellValue;
+                case CellType.Numeric: //NUMERIC:  
+                    return cell.NumericCellValue;
+                case CellType.String: //STRING:  
+                    return cell.StringCellValue;
+                case CellType.Error: //ERROR:  
+                    return cell.ErrorCellValue;
+                case CellType.Formula: //FORMULA:  
+                default:
+                    return "=" + cell.CellFormula;
+            }
+        }
+        
+
+
+    }
+}
--- UI/ashx_ui/INVTA.ashx.cs
+++ UI/ashx_ui/INVTA.ashx.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.SessionState;
+using YJUI.Common;
+using YJUI.DBUtility;
+
 
 namespace YJUI.UI.ashx_ui
 {
@@ -24,12 +28,93 @@ namespace YJUI.UI.ashx_ui
             else if (context.Request.Params["action"] == "searchErpNum")
             {
                 var danbie = context.Request.Params["orderId"];
-                
+
                 var date = context.Request.Params["tim"];
-                date = date.Replace("-","");
+                date = date.Replace("-", "");
                 string strjson = new BLL.INVTA().GetInvtaErpNum(danbie, date);
                 context.Response.Write(strjson);
                 context.Response.End();
+            }
+            #endregion
+
+            #region 批量导入操作
+            else if (context.Request.Params["action"] == "import")
+            {
+
+                HttpFileCollection files = context.Request.Files;
+                //if (files.Count > 0)
+                //{
+                //    context.Response.Write("有文件");
+
+                //}
+                HttpPostedFile hpFile = context.Request.Files[0];
+
+
+                string filePath = "../UploadFile";
+                if (hpFile.ContentLength > 0)
+                {
+                    string fileName = System.IO.Path.GetFileName(hpFile.FileName);
+                    filePath = context.Server.MapPath(filePath + "/" + fileName);
+                    hpFile.SaveAs(filePath);
+                    // DataTable dt = ExcelHelper.ExcelToDataSet("sheet1", filePath);
+                    DataTable dt1 = ExcelHelper.ExcelToTable(filePath);
+                    DataTable dtC = new DataTable();
+                    dtC.Columns.Add("品号");
+                    dtC.Columns.Add("品名");
+                    dtC.Columns.Add("规格");
+                    dtC.Columns.Add("OEM");
+                    dtC.Columns.Add("数量");
+                    dtC.Columns.Add("单位");
+                    dtC.Columns.Add("小单位");
+                    dtC.Columns.Add("单位成本");
+                    dtC.Columns.Add("金额");
+                    dtC.Columns.Add("仓库");
+                    dtC.Columns.Add("仓库名称");
+                    dtC.Columns.Add("批号");
+                    dtC.Columns.Add("品号说明");
+                    dtC.Columns.Add("生产日期");
+                    dtC.Columns.Add("有效日期");
+                    dtC.Columns.Add("复检日期");
+                    dtC.Columns.Add("项目编号");
+                    dtC.Columns.Add("备注");
+
+
+                    string[] arrPinhao = dt1.AsEnumerable().Select(d => d.Field<string>("品号")).ToArray();
+                    string s = "'" + string.Join("','", dt1.AsEnumerable().Select(d => d.Field<string>("品号")).ToArray()) + "'";
+                    string sql = string.Format("select MB001,MB002,MB003,MB004,MB064,MB065,MB072 from INVMB where MB001 in({0})", s);
+                    DataTable dataTable = VOCEN2018DbHelperSQL.Query(sql).Tables[0];
+                    var query = from t in dt1.AsEnumerable()
+                                join t2 in dataTable.AsEnumerable() on t.Field<string>("品号") equals t2.Field<string>("MB001").Trim()
+                                into g from t2 in g.DefaultIfEmpty()
+                                select new
+                                {
+                                    pinhao = t.Field<string>("品号"),
+                                    pinming = t.Field<string>("品名"),
+                                    guige = t.Field<string>("规格"),
+                                    oem = "",
+                                    shuliang=t.Field<string>("调账数量"),
+                                    dw= t2.Field<string>("MB004"),
+                                    xdw = "",
+                                    chengben= "",
+                                    money= "",//金额
+                                    cangku = t.Field<string>("仓库编码"),
+                                    ckName = "",
+                                    pihao ="",
+                                    pihaoshuoming ="",
+                                    scDate="",
+                                    yxDate="",
+                                    fjDate="",
+                                    xmbh="",
+                                    bz = t.Field<string>("备注"),
+                                };
+                    query.ToList().ForEach(q => dtC.Rows.Add(q.pinhao,q.pinming,q.guige,q.oem,q.shuliang,q.dw,q.xdw,q.chengben,q.money,q.cangku,q.ckName,q.pihao,q.pihaoshuoming,q.scDate,q.yxDate,q.fjDate,q.xmbh,q.bz));
+                    string strjson = Common.JsonHelper.ToJson(dtC);
+
+                    context.Response.Write("{\"rows\":" + strjson + "}");
+
+                }
+
+
             }
             #endregion 
         }
